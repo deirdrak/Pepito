64029e62aee6e9953c3a7ba4827eac748b6069ac
Wendy Paz <deirdrak@gmail.com>
2011-10-07 20:30:40 -0600

    Algo mas

---
+++ src/pepito/Wendy.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pepito;
+
+/**
+ *
+ * @author Wendy
+ */
+public class Wendy {
+    public static void main(String args[]){
+        System.out.println("WENDY!!!!!");
+    }
+}
